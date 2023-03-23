c896d5fb51564c16f78523815f9da1861f224991
dennisappiahALX <appiahdennis1998@gmail.com>
2023-03-23 16:08:34 +0000

    register routes

---
+++ startUp/registerRoutes.js
@@ -0,0 +1,29 @@
+import express from "express";
+import userRoute from "../routes/user.route.js";
+import gigRoute from "../routes/gig.route.js";
+import orderRoute from "../routes/order.route.js";
+import conversationRoute from "../routes/conversation.route.js";
+import messageRoute from "../routes/conversation.route.js";
+import reviewRoute from "../routes/review.route.js";
+import authRoute from "../routes/auth.route.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import { errorHandler } from "../middleware/errorHandler.js";
+
+
+export const setUpRoutes = (app) => {
+  app.use(express.json());
+  app.use(cors());
+  app.use(express.urlencoded({ extended: true }));
+  app.use(cookieParser());
+  app.use("/api/auth", authRoute);
+  app.use("/api/users", userRoute);
+  app.use("/api/gigs", gigRoute);
+  app.use("/api/orders", orderRoute);
+  app.use("/api/conversations", conversationRoute);
+  app.use("/api/messages", messageRoute);
+  app.use("/api/reviews", reviewRoute);
+  app.use(errorHandler);
+};
+
+
