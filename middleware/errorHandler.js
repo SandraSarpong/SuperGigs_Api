66341cab86a43da4e4d5e7cbc6f21599a38aace5
dennisappiahALX <appiahdennis1998@gmail.com>
2023-03-24 22:46:59 +0000

    dele

--- middleware/errorHandler.js
+++
@@ -1,8 +0,0 @@
-export const errorHandler = (error, req, res, next) => {
-  // log errors from application
-  console.error(error.status);
-  console.error(error.message);
-
-  // handle all other errors
-  res.status(500).send("Something went wrong");
-};
--- startUp/intergrateDb.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-import "dotenv/config";
-
-export const setupDb = () => {
-  mongoose
-    .connect(`${process.env.DB}`)
-    .then(() => console.log(`Connected to ${process.env.DB}`))
-    .catch((err) => console.log("Could not connect to MongoDb..", err));
-};
