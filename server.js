b1007ba7f3d4bddb1f86cc882067856ac8c75f90
Philip Ladzaka <philipkelly407@gmail.com>
2023-03-30 03:09:27 +0000

    reverted fix

--- server.js
+++ server.js
@@ -5,10 +5,6 @@ import cors from "cors";
 
 const app = express();
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
-app.use(express.json());
-app.use(cookieParser());
-
 setUpRoutes(app);
 setupDb();
 
