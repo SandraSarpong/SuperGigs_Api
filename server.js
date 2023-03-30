a254e27aa6c81cd1e3b764d9e79accee3efc0757
Philip Ladzaka <philipkelly407@gmail.com>
2023-03-30 02:50:17 +0000

    enabled cors

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 import express from "express";
 import { setUpRoutes } from "./startUp/registerRoutes.js";
 import { setupDb } from "./startUp/intergrateDb.js";
+import cors from "cors";
 
 const app = express();
+
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(express.json());
+app.use(cookieParser());
 
 setUpRoutes(app);
 setupDb();
