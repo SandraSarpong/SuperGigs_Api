6517a0858da8338345a5934e3ee6513855e34de1
Philip Ladzaka <philipkelly407@gmail.com>
2023-03-30 03:02:12 +0000

    new fix

--- startUp/registerRoutes.js
+++ startUp/registerRoutes.js
@@ -19,7 +19,7 @@ export const setUpRoutes = (app) => {
 //     'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
 //     'preflightContinue': false
 //   }));
-  app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+  app.use(cors({ origin: "http://127.0.0.1:5173", credentials: true }));
   app.use(express.json());
   app.use(cookieParser());
   app.use(express.urlencoded({ extended: true }));
